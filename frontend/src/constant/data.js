0079e04e428eeeabd8b5acea10445ebcaf87c3c4
Abishek <abishekvarm8929@gmail.com>
2024-02-13 15:22:38 +0530

    commit

--- frontend/src/constant/data.js
+++ frontend/src/constant/data.js
@@ -19,62 +19,6 @@ export const listOfProduct = [
                 item_note: "", // Add the actual note for Avocado if available
                 item_category_id: "pwqU4JAHXUEF2GKWQ",
             },
-            {
-                item_id: "5f0a02eb-ed23-4b95-b556-f46439eded99",
-                item_name: "Acocado",
-                item_url: "url", // Replace with the actual URL
-                item_note: "", // Add the actual note for Avocado if available
-                item_category_id: "pwqU4JAHXUEF2GKWQ",
-            },
-            {
-                item_id: "77ed0787-8519-4bde-a55f-c507d3d90a7e",
-                item_name: "Banana",
-                item_url: "url", // Replace with the actual URL
-                item_note: "", // Add the actual note for Banana if available
-                item_category_id: "pwqU4JAHXUEF2GKWQ",
-            },
-            {
-                item_id: "pwqVSKYRLV2QQH7WQ",
-                item_name: "Acocado",
-                item_url: "url", // Replace with the actual URL
-                item_note: "", // Add the actual note for Avocado if available
-                item_category_id: "pwqU4JAHXUEF2GKWQ",
-            },
-            {
-                item_id: "pwqQ6EOBNEHYQOKWQ",
-                item_name: "Piele De Sapo",
-                item_url: "", // Replace with the actual URL
-                item_note: "", // Add the actual note for Piele De Sapo if available
-                item_category_id: "pwqU4JAHXUEF2GKWQ",
-            },
-            {
-                item_id: "pwqXVBQ7B8GIT89WQ",
-                item_name: "Bunch of carrots 5pcs",
-                item_url: "url", // Replace with the actual URL
-                item_note: "", // Add the actual note for Avocado if available
-                item_category_id: "pwqU4JAHXUEF2GKWQ",
-            },
-            {
-                item_id: "pwqFMI3ER5IE7SZWQ",
-                item_name: "Pre-cooked",
-                item_url: "url", // Replace with the actual URL
-                item_note: "", // Add the actual note for Avocado if available
-                item_category_id: "pwqU4JAHXUEF2GKWQ",
-            },
-            {
-                item_id: "pwqZY1RMRCLLWY0WQ",
-                item_name: "Mandarin Nadorcott",
-                item_url: "url", // Replace with the actual URL
-                item_note: "", // Add the actual note for Avocado if available
-                item_category_id: "pwqU4JAHXUEF2GKWQ",
-            },
-            {
-                item_id: "pwqVCMH6UVATW6DWQ",
-                item_name: "Watermelon",
-                item_url: "url", // Replace with the actual URL
-                item_note: "", // Add the actual note for Watermelon if available
-                item_category_id: "pwqU4JAHXUEF2GKWQ",
-            },
         ],
     },
     {
@@ -83,34 +27,6 @@ export const listOfProduct = [
         productslist: [
             {
                 item_id: "dsafF1776AZUZKTT4781wdd2",
-                item_name: "Chicken",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf30S60T3AYYPGYEN1wdd2",
-            },
-            {
-                item_id: "dsafU9QLIQ2CRJP62MO1wdd2",
-                item_name: "Chicken leg box",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf30S60T3AYYPGYEN1wdd2",
-            },
-            {
-                item_id: "563e06b9-90f4-474d-a693-d1108bfe72aa",
-                item_name: "Chicken",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf30S60T3AYYPGYEN1wdd2",
-            },
-            {
-                item_id: "43769e67-d546-46b9-98ce-1e87f65dd333",
-                item_name: "Chicken",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf30S60T3AYYPGYEN1wdd2",
-            },
-            {
-                item_id: "2514d903-6d88-48d1-b019-983a4c8f6511",
                 item_name: "Chicken",
                 item_url: "url",
                 item_note: "",
@@ -132,69 +48,6 @@ export const listOfProduct = [
             {
                 item_id: "dsafJFSGWLSWU2DOTU11wdd2",
                 item_name: "Beverage1",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
-            },
-            {
-                item_id: "dsafJGOR1GHNURBMZDQ1wdd2",
-                item_name: "Beverage2",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
-            },
-            {
-                item_id: "acf60783-e5ad-4454-afbf-a7a29bf38b41",
-                item_name: "Beverage3",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
-            },
-            {
-                item_id: "393e10a7-ab5f-4417-b284-cbce179fe5e3",
-                item_name: "Beverage4",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
-            },
-            {
-                item_id: "927a553f-3e0d-4928-8857-2055ba0c4b0c",
-                item_name: "Beverage5",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
-            },
-            {
-                item_id: "e91db792-c290-4a56-9d23-a10159a09f96",
-                item_name: "Beverage6",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
-            },
-            {
-                item_id: "0c74bfd7-2ab1-41ef-a48a-12219a2a894d",
-                item_name: "Beverage7",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
-            },
-            {
-                item_id: "a4a7f561-4029-47f2-a612-dedf75e65eef",
-                item_name: "Beverage8",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
-            },
-            {
-                item_id: "8b00d861-43e5-4da7-94a4-f85c49742981",
-                item_name: "Beverage9",
-                item_url: "url",
-                item_note: "",
-                item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
-            },
-            {
-                item_id: "24514223-7388-4398-a4c3-66ae03173b50",
-                item_name: "Beverage10",
                 item_url: "url",
                 item_note: "",
                 item_category_id: "dsaf4FIKEV61QDFTDGQ1wdd2",
@@ -244,12 +97,6 @@ export const shoppingList = [
                 category_id: "1eqewfzfew",
                 quantity: 1,
             },
-            {
-                item_name: "Apple1",
-                item_id: "dafa2",
-                category_id: "1eqewfzfew",
-                quantity: 1,
-            },
         ],
     },
     {
@@ -262,18 +109,7 @@ export const shoppingList = [
                 category_id: "qwwr32444",
                 quantity: 1,
             },
-            {
-                item_name: "chicken2",
-                item_id: "dafa23",
-                category_id: "qwwr32444",
-                quantity: 1,
-            },
-            {
-                item_name: "chicken6",
-                item_id: "dafa16",
-                category_id: "qwwr32444",
-                quantity: 1,
-            },
+
         ],
     },
 ];
