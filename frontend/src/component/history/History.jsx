c7eab79e0328db21892177c86b58ce24fcef2100
Abishek <abishekvarm8929@gmail.com>
2024-02-29 08:28:54 +0530

    commit

--- frontend/src/component/history/History.jsx
+++ frontend/src/component/history/History.jsx
@@ -1,58 +1,21 @@
 import "./history.css";
-import { PiCalendar } from "react-icons/pi";
-import { RiArrowRightSLine } from "react-icons/ri";
+import CardContainer from "../cardContainer/CardContainer";
+import { history } from "../../constant/data";
 
 const History = () => {
+    
     return (
+
         <div className="history_container">
-            <h1>Shopping history</h1>
+            <h1>{history?.history_container?.title}</h1>
 
-            <div className="history">
-                <p>August 2020</p>
-
-                <div className="card">
-                    <h5>Grocery List</h5>
-
-                    <div className="card_container">
-                        <div className="calendar_container">
-                            <PiCalendar size={20} color="#C1C1C4" />
-                            <span>Mon 27.8.2020</span>
-                        </div>
-                        <button className="completedbtn">completed</button>
-                        <RiArrowRightSLine size={26} color="#F9A109"/>
-                    </div>
+            {history?.history_container?.history.map((data,index) => (
+                <div className="history" key={index}>
+                     <p>{data.date}</p>
+                    <CardContainer list={data.items} key={index+data.date}/>
                 </div>
+            ))}
 
-                <div className="card">
-                    <h5>Eero’s farewell party</h5>
-
-                    <div className="card_container">
-                        <div className="calendar_container">
-                            <PiCalendar size={20} color="#C1C1C4" />
-                            <span>Mon 24.8.2020</span>
-                        </div>
-                        <button className="completedbtn">completed</button>
-                        <RiArrowRightSLine size={26} color="#F9A109"/>
-                    </div>
-                </div>
-            </div>
-
-            <div className="history">
-                <h4>July 2020 2020</h4>
-
-                <div className="card">
-                    <h5>Grocery List</h5>
-
-                    <div className="card_container">
-                        <div className="calendar_container">
-                            <PiCalendar size={20} color="#C1C1C4" />
-                            <span>Mon 16.7.2020</span>
-                        </div>
-                        <button className="cancelbtn">cancelled</button>
-                        <RiArrowRightSLine size={26} color="#F9A109"/>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 };
